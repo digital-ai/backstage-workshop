681296848e2b189323517d83a9a18730db94d3e2
Nithesh <nithesh.vaddey@digital.ai>
2024-10-30 14:37:41 +0530

    S-114685: Changes to Front end tempaltes added

--- backstage-with-new-backend/packages/app/src/App.tsx
+++ backstage-with-new-backend/packages/app/src/App.tsx
@@ -37,7 +37,7 @@ import { AppRouter, FlatRoutes } from '@backstage/core-app-api';
 import { CatalogGraphPage } from '@backstage/plugin-catalog-graph';
 import { RequirePermission } from '@backstage/plugin-permission-react';
 import { catalogEntityCreatePermission } from '@backstage/plugin-catalog-common/alpha';
-import {DaiReleasePage, } from '@digital-ai/plugin-dai-release';
+import { DaiReleasePage,DaiTemplatePage } from '@digital-ai/plugin-dai-release';
 
 
 const app = createApp({
@@ -103,7 +103,7 @@ const routes = (
     <Route path="/settings" element={<UserSettingsPage />} />
     <Route path="/catalog-graph" element={<CatalogGraphPage />} />
     <Route path="/dai-release" element={<DaiReleasePage/>}/>
-    <Route path="/dai-template" element={<DaiReleasePage/>}/>
+    <Route path="/dai-template" element={<DaiTemplatePage/>}/>
   </FlatRoutes>
 );
 
